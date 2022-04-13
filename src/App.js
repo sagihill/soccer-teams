59549dfb9e4fafd5666c25b2887c9b3d80bef094
Sagi Hillel <sagi@noosa.io>
2022-04-13 18:32:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TeamsTable } from "./components/TeamsTable/TeamsTable";
+// import "./App.styles.js";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TeamsTable></TeamsTable>
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/TeamsTable/TeamsTable.js
@@ -0,0 +1,5 @@
+import { TableComponent } from "./TeamsTable.styles";
+
+export const TeamsTable = () => {
+  return <TableComponent>HELLLLO!</TableComponent>;
+};
