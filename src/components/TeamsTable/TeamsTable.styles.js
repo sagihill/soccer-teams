1bdaba99b05974fab1caaac200fb2be2e05d6559
Sagi Hillel <sagi@noosa.io>
2022-04-20 10:09:44 +0300

    fixed shrink

--- src/components/TeamsTable/TeamsTable.styles.js
+++ src/components/TeamsTable/TeamsTable.styles.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const TableComponent = styled.div`
   display: flex;
   flex-direction: column;
-  width: 60%;
+  width: 60rem;
   border: solid 1px black;
 `;
